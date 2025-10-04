1f45791bfd411d69613063e419729382f7299192
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-10-05 00:07:38 +0530

    express and server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+//server setup
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//middleware 
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.port || 5000;
+app.listen(port, () => {
+console.log(`server is running`);
+});
